d0292d5638cb9b3fe646ae2409567d93b0dc4665
VIJAY_ECE2026 <vijaydurairaj2005@gmail.com>
2025-01-03 19:25:21 +0530

    update

--- src/pages/onboarding.jsx
+++ src/pages/onboarding.jsx
@@ -37,7 +37,7 @@ const Onboarding = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-40">
       <h2 className="gradient-title font-extrabold text-7xl sm:text-8xl tracking-tighter">
-        I am a...
+        I am a....
       </h2>
       <div className="mt-16 grid grid-cols-2 gap-4 w-full md:px-40">
       <Button
